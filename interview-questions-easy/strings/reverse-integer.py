17b7eddb78360115205299f7ba9718ec12d5b65e
holden <htwight@gmail.com>
2019-05-14 17:33:26 -0400

    solve reverse-integer (python3)

---
+++ interview-questions-easy/strings/reverse-integer.py
@@ -0,0 +1,24 @@
+from collections import deque
+class Solution:
+    def reverse(self, x: int) -> int:
+        negative = True if x < 0 else False
+        num = abs(x)
+        queue = deque([])
+        while (num != 0):
+            queue.append(num % 10)
+            num = num // 10
+
+        result = 0
+        base = [10**x for x in range(len(queue))]
+        for power in base:
+            result = result + queue.pop() * power
+
+        if result > 2**31 - 1 or result < -(2**31):
+            return 0
+        else:
+            return -result if negative else result
+
+
+s = Solution()
+print(s.reverse(2147483641))
+print(s.reverse(6463847412))
